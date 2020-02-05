352edfe57394d8f966e02638865a83c53df68771
Seth Holobaugh <seth.holobaugh@gmail.com>
2020-02-05 12:16:09 -0600

    Added tailwind to Vue template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>Hello There</h1>
+  <div class="h-screen w-width bg-blue-300 flex flex-col justify-center items-center">
+    <div class="header">
+      <h1 class="text-4xl">Seth's Todo App With Tailwind</h1>
+      <div class="form flex">
+        <input class="appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-non" id="newText" type="text" placeholder="Add New Todo"/>
+        <button id="newBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">Add</button>
+      </div>
+    </div>
+    <div class="todos w-10/12 mt-8">
+      <div class="pendings">
+        <h3 class="text-2xl">Pendings:</h3>
+        <ul id="pendingList">
+          <li class="text-center p-3 bg-white mt-4 rounded shadow-lg cursor-pointer hover:bg-blue-500">Item 1</li>
+          <li class="text-center p-3 bg-white mt-4 rounded shadow-lg cursor-pointer hover:bg-blue-500">Item 2</li>
+        </ul>
+        </div>
+      <div class="completed mt-8">
+        <h3 class="text-2xl">Completed:</h3>
+        <ul id="completedList">
+          <li class="text-center p-3 bg-white line-through text-red-500 mt-4 rounded shadow-lg cursor-pointer hover:bg-red-500 hover:text-black">Item 3</li>
+          <li class="text-center p-3 bg-white line-through text-red-500 mt-4 rounded shadow-lg cursor-pointer hover:bg-red-500 hover:text-black">Item 4</li>
+        </ul>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
